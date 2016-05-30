3dc0e14dc3d68f329ab3af6ff743c9ae7c703806
Michael Bewley <m.bewley@acfr.usyd.edu.au>
2016-05-30 21:27:36 +1000

    Added some basic code to grab patches from images, read data from a data set file, and write out patches named by class label.

---
+++ prep/patches.py
@@ -0,0 +1,59 @@
+import os
+import cv2
+import pandas as pd
+
+def get_image(image_path):
+    if os.path.exists(image_path):
+        im = cv2.imread(image_path)[::-1]  # Reverse channels to make BGR into RGB
+        return im
+    else:
+        raise IOError("Couldn't find image {}".format(image_path))
+
+def get_patches_from_image(im, coords_index, patch_size, return_patches=True):
+    """
+
+
+    :param withempty: Whether to include 'na' patch entries in the index.
+    :param im: An image as a numpy array. Patch will be taken from all channels, as (rows, columns, channels)
+    :param coords_index: Index of (row, col) points to use as patch centres
+    :param patch_size: Size of patches in pixels (e.g. 15 = square 15x15 patches)
+    :param return_patches: Whether to return the actual patches, or just the index of valid patches
+    :return: An index of valid patches (if return_patches), or a pandas series of numpy array patches, indexed by
+            coords_index.
+    """
+    assert (patch_size % 2 == 1) and (patch_size > 0)
+    patch_radius = patch_size / 2
+
+    r = coords_index.get_level_values('row').values
+    c = coords_index.get_level_values('col').values
+
+    patches = pd.DataFrame(index=coords_index)
+    patches['min_r'] = r - patch_radius
+    patches['max_r'] = r + patch_radius
+    patches['min_c'] = c - patch_radius
+    patches['max_c'] = c + patch_radius
+
+    patches['not_cropped'] = (patches.min_r >= 0) & (patches.max_r < im.shape[0]) & \
+                             (patches.min_c >= 0) & (patches.max_c < im.shape[1])
+    patches.loc[patches.min_r < 0, 'min_r'] = 0
+    patches.loc[patches.min_c < 0, 'min_c'] = 0
+    image_patches = {}
+
+    if not return_patches:
+        return patches.index
+    else:
+        for (r, c), p in patches.iterrows():
+            image_patches[(r,c)] = im[p.min_r:p.max_r + 1, p.min_c:p.max_c + 1]
+        return patches, image_patches
+
+def write_patches_as_images(image_patches, labels, out_dir):
+    """
+
+    :param image_patches: Dictionary of key (r,c), value (patch RGB numpy array)
+    :param labels: Dictionary of key (r,c), value (class labels)
+    :param out_dir: Directory to write images
+    :return:
+    """
+    for r, c in image_patches:
+        label = labels[(r,c)]
+        cv2.imwrite(os.path.join(out_dir, '{}.png'.format(label)), image_patches[(r,c)])
---
+++ prep/tests.py
@@ -0,0 +1,36 @@
+import os
+import logging
+
+import cv2
+import pandas as pd
+
+from benthoz.prep.patches import get_image, get_patches_from_image, write_patches_as_images
+
+DATA_SPLIT_DIR = '../data_splits'
+IMAGES_DIR = '../data/benthoz-2015'
+OUT_DIR = '../data/output_patches'
+
+logging.root.setLevel(logging.INFO)
+images_list = []
+
+for data_split_file in os.listdir(DATA_SPLIT_DIR):
+    data_split = pd.read_csv(os.path.join(DATA_SPLIT_DIR, data_split_file))
+    images_list.extend(data_split.image_name.unique())
+logging.info('Data splits refers to %d unique images' % len(images_list))
+print(images_list[0])
+images_found = [os.path.splitext(p)[0] for p in os.listdir(IMAGES_DIR)]
+logging.info('Image files found locally: %d' % len(images_found))
+
+training_points = pd.read_csv(os.path.join(DATA_SPLIT_DIR, 'public_labels_train.csv'))
+
+for image_name, group in training_points.groupby('image_name'):
+    try:
+        im = get_image(os.path.join(IMAGES_DIR, image_name+'.png'))
+    except IOError as e:
+        logging.debug("Couldn't find image {}".format(image_name))
+    else:
+        group = group.set_index(['row', 'col'])
+        print(group)
+        p, image_patches = get_patches_from_image(im, group.index, 127)
+        write_patches_as_images(image_patches, group.label, OUT_DIR)
+
